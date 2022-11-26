b17dcdfa60cfe62750addabf87fdfe76881ff01b
Shivraj Singh <103109048+Shivraj-TaechBuddy@users.noreply.github.com>
2022-11-26 11:15:45 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,22 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print("Welcoem to word game called :- \n \n","""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -13,7 +30,8 @@ def encrypt(plain_text, shift_number):
     new_posstion= possition+ shift_number
     new_letter = alphabet[new_posstion]
     cipher_text += new_letter
-    print(f"your new encoded text is {cipher_text}")
+  print(f"your new encoded text is {cipher_text}")
+  
 
 
 def decrypt(cipher_text, shift_number):
@@ -23,7 +41,7 @@ def decrypt(cipher_text, shift_number):
     new_posstion= possition- shift_number
     new_letter = alphabet[new_posstion]
     plain_text += new_letter
-    print(f"your new encoded text is {plain_text}")    
+  print(f"your new encoded text is {plain_text}")    
     
     
 
